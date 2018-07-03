0ff65805d02bf9a9ebc89be93e66f20a61e6428d
h00pak <robbyglasco@sbcglobal.net>
2018-07-03 08:49:46 -0500

    Add files via upload
    try and catch around 103

--- index.js
+++ index.js
@@ -100,7 +100,11 @@ DirectvSTBAccessory.prototype = {
 						remote.getTuned(that.clientAddr, function (err, response) {
 							if (!err) {
 								that.log.debug('DTV location %s current channel is %d', that.location, parseInt(response.major));
-								callback(null, parseInt(response.major));
+								try{
+									callback(null, parseInt(response.major));
+								} catch (e) {
+									
+								}
 							} else {
 								that.log.debug('Unable to call for current channel at DTV location %s.', that.location);
 								callback(null, parseInt("0"));
